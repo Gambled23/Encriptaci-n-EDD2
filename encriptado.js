1e26267170404248223ee4e97b3211f55e5c9204
Cesar <ipog71@gmail.com>
2022-11-08 11:48:31 -0600

    encriptado y desencriptado cesar

--- encriptado.js
+++ encriptado.js
@@ -1,7 +1,64 @@
 console.log('ping')
 
-function encriptar() {
-    let txtDesInput = document.getElementById('txtEncriptar').value
+const textoTraducido = document.getElementById('textoTraducido')
+const aux = document.getElementById('xd')
 
+function encriptar() {
+    //Cesar
+    let txtDesInput = document.getElementById('txtEncriptar').value
+    let textoNuevo = cifradoCesar(txtDesInput)
+    //XOR
     
+
+
+
+
+
+    textoTraducido.innerHTML = textoNuevo
+    
+}
+
+function desencriptar() {
+    let txtDesInput = document.getElementById('txtEncriptar').value
+    textoDesencriptado = descifradoCesar(txtDesInput)
+}
+
+function cifradoCesar(txt) {
+    let letra, textoCifrafo = ''
+    let alfabeto = 'abcdefghijklmnñopqrstuvwxyz';
+    let posiciones = 8;
+    //Rotamos el alfabeto 8 lugares a la derecha
+    let cifrado = alfabeto.slice(-posiciones);
+    cifrado += alfabeto.slice(0, alfabeto.length - posiciones);
+    //Encriptar recorriendo 8 posiciones
+    for (let i = 0; i < txt.length; i++) {
+        letra = txt[i].toLowerCase();
+        if (letra == ' ') {
+            letra = ' ';
+        }
+        else {
+            letra = cifrado[alfabeto.indexOf(letra)];
+        }
+        textoCifrafo += letra;
+    }
+    return textoCifrafo
+}
+
+function descifradoCesar(txt) {
+    let letra, respuesta = '';
+    let alfabeto = 'abcdefghijklmnñopqrstuvwxyz'
+    let clave = 0
+    let cifrado = alfabeto.slice(-clave);
+    cifrado += alfabeto.slice(0, alfabeto.length - clave)
+    for (let i = 0; i < txt.length; i++) {
+        letra = txt[i].toLowerCase();
+        if (letra == ' ') {
+            letra = ' ';
+        }
+        else {
+            letra = alfabeto[cifrado.indexOf(letra)];
+        }
+        respuesta += letra;
+    }
+    return respuesta;
 }
