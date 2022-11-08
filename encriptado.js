db8e091c20d9dc0c1400ff272d7f2545ad90fcc3
Cesar <ipog71@gmail.com>
2022-11-08 12:30:42 -0600

    puntos y comas

--- encriptado.js
+++ encriptado.js
@@ -21,55 +21,54 @@ function desencriptar() {
 
 function cifradoCesar(txt) {
     let letra, textoCifrafo = ''
-    let alfabeto = 'abcdefghijklmnñopqrstuvwxyz';
-    let posiciones = 8;
+    let alfabeto = 'abcdefghijklmnñopqrstuvwxyz'
+    let posiciones = 8
     //Rotamos el alfabeto 8 lugares a la derecha
-    let cifrado = alfabeto.slice(-posiciones);
+    let cifrado = alfabeto.slice(-posiciones)
     cifrado += alfabeto.slice(0, alfabeto.length - posiciones);
     //Encriptar recorriendo 8 posiciones
     for (let i = 0; i < txt.length; i++) {
-        letra = txt[i].toLowerCase();
+        letra = txt[i].toLowerCase()
         if (letra == ' ') {
-            letra = ' ';
+            letra = ' '
         }
         else {
             letra = cifrado[alfabeto.indexOf(letra)];
         }
-        textoCifrafo += letra;
+        textoCifrafo += letra
     }
     return textoCifrafo
 }
 
 function descifradoCesar(txt) {
-    let letra, respuesta = '';
+    let letra, respuesta = ''
     let alfabeto = 'abcdefghijklmnñopqrstuvwxyz'
     let clave = 8
-    let cifrado = alfabeto.slice(-clave);
+    let cifrado = alfabeto.slice(-clave)
     cifrado += alfabeto.slice(0, alfabeto.length - clave)
     for (let i = 0; i < txt.length; i++) {
-        letra = txt[i].toLowerCase();
+        letra = txt[i].toLowerCase()
         if (letra == ' ') {
-            letra = ' ';
+            letra = ' '
         }
         else {
-            letra = alfabeto[cifrado.indexOf(letra)];
+            letra = alfabeto[cifrado.indexOf(letra)]
         }
-        respuesta += letra;
+        respuesta += letra
     }
-    return respuesta;
+    return respuesta
 }
 
 function cifradoXor(txt)
 {
-    txt = txt.split("");
+    txt = txt.split("")
     let xorKey = 'P'; //Clave XOR
-    let longitud = txt.length;
+    let longitud = txt.length
  
     //operacion xor con cada caracter
     for (let i = 0; i < longitud; i++)
     {
-        txt[i] = (String.fromCharCode((txt[i].charCodeAt(0)) ^ xorKey.charCodeAt(0)));
-        console.log(txt[i])
+        txt[i] = (String.fromCharCode((txt[i].charCodeAt(0)) ^ xorKey.charCodeAt(0)))
     }
-    return txt.join("");
+    return txt.join("")
 }
